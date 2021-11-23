26c6a452fd6b58caa5af5248ce03c0557dfc0200
ojullien <3778194+ojullien@users.noreply.github.com>
2021-11-23 21:01:49 +0100

    fix: extends sequence parameter types, psr naming convention
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- src/Definition/SequenceInterface.php
+++ src/Definition/SequenceInterface.php
@@ -12,7 +12,7 @@ namespace OJullien\ContainerBuilderBridge\Definition;
 
 /**
  * The interface for all sequences.
- * @extends \IteratorAggregate<string,string|callable>
+ * @extends \IteratorAggregate<string,array|bool|callable|int|float|string>
  */
 interface SequenceInterface extends \Countable, \IteratorAggregate
 {
@@ -27,13 +27,16 @@ interface SequenceInterface extends \Countable, \IteratorAggregate
      * Adds a definition to the sequence.
      *
      * examples:
-     * ->add(Acme\Foo::class)
-     * ->add(Acme\BarInterface::class,Acme\BarA::class)
-     * ->add('service',function...)
+     * ->withDefinition(Acme\Foo::class)
+     * ->withDefinition(Acme\BarInterface::class,Acme\BarA::class)
+     * ->withDefinition('service',function...)
+     * ->withDefinition('database.host', 'localhost')
+     * ->withDefinition('database.port', 5000)
+     * ->withDefinition('report.recipients', ['bob@example.com','alice@example.com',])
      *
      * @param string $alias Alias or classname
-     * @param string|callable|null $definition
+     * @param array<array-key,scalar>|bool|callable|int|float|string|null $definition
      * @return static
      */
-    public function withDefinition(string $alias, string|callable|null $definition = null): static;
+    public function withDefinition(string $alias, array|bool|callable|int|float|string|null $definition = null): static;
 }
--- tests/ContainerBuilderBridge/AbstractionTest.php
+++ tests/ContainerBuilderBridge/AbstractionTest.php
@@ -4,25 +4,47 @@ declare(strict_types=1);
 
 namespace OJullienTest\ContainerBuilderBridge;
 
-use OJullienTest\Utils\StubBridge;
-use OJullienTest\Utils\StubBuilder;
+use OJullien\ContainerBuilderBridge\Definition\Sequence;
+use OJullienTest\Utils\AbstractionStub;
+use OJullienTest\Utils\ImplementationStub;
 use Psr\Container\ContainerInterface;
 
 class AbstractionTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @covers \OJullien\ContainerBuilderBridge\Abstraction
+     * @covers \OJullien\ContainerBuilderBridge\AbstractBuilder
+     * @uses \OJullien\ContainerBuilderBridge\Definition\Sequence
      * @group specification
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
      * @throws \PHPUnit\Framework\Exception
      * @return void
      */
-    public function testCreateContainer(): void
+    public function testGetContainer(): void
     {
-        $pBuilder = new StubBuilder();
-        $pBridge = new StubBridge($pBuilder);
+        // Set sequence of definitions
+        $pSequence1 = Sequence::getSequence();
+        $pSequence1 = $pSequence1->withDefinition('solo')
+            ->withDefinition('service', \stdClass::class)
+            ->withDefinition('api.url', 'http://api.example.com')
+            ->withDefinition('webservice', function () {
+                return new \stdClass();
+            });
+
+        $array = ['bob@example.com', 'alice@example.com',];
+        $pSequence2 = Sequence::getSequence();
+        $pSequence2 = $pSequence2->withDefinition('database.host', 'localhost')
+            ->withDefinition('database.port', 5000)
+            ->withDefinition('report.recipients', $array);
+
+        // Set a builder implementation
+        $pBuilder = new ImplementationStub();
+
+        // Set a bridge
+        $pBridge = new AbstractionStub($pBuilder);
         $pBridge->setContainerBuilder($pBuilder);
-        $pContainer = $pBridge->build();
+
+        // Get container
+        $pContainer = $pBridge->getContainer($pSequence1, $pSequence2);
         self::assertInstanceOf(ContainerInterface::class, $pContainer);
     }
 }
